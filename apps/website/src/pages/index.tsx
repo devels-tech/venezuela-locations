4761205466cdd768c1bbd23538b9ca75612752eb
kevelscy <kevelscy@gmail.com>
2023-04-24 01:39:02 -0400

    update link locations docs

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
         <a
-          href="https://venezuela-locations.vercel.app"
+          href="https://locations-doc.devels.tech/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
