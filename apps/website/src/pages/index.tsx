2d8934687ec56fc7cdd6849f6d156b6a5e7c090e
kevelscy <kevelscy@gmail.com>
2023-05-11 14:58:29 -0400

    comment Pro Plan

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -16,21 +16,21 @@ export default function Home() {
       ctaName: "Comenzar a usar",
       ctaLink: "https://locations-doc.devels.tech",
       isPro: false,
-    },
-    {
-      planName: "Pro",
-      planCost: "0",
-      planDescription:
-        "Para empresas que deseen utilizar todo el potencial de nuestra API",
-      planFeatures: [
-        "Edge",
-        "Informacion en multiples formatos",
-        "Hasta 10000 requests por dia",
-      ],
-      ctaName: "Comenzar a usar",
-      ctaLink: "https://locations-doc.devels.tech",
-      isPro: true,
-    },
+    }
+    // {
+    //   planName: "Pro",
+    //   planCost: "0",
+    //   planDescription:
+    //     "Para empresas que deseen utilizar todo el potencial de nuestra API",
+    //   planFeatures: [
+    //     "Edge",
+    //     "Informacion en multiples formatos",
+    //     "Hasta 10000 requests por dia",
+    //   ],
+    //   ctaName: "Comenzar a usar",
+    //   ctaLink: "https://locations-doc.devels.tech",
+    //   isPro: true,
+    // },
   ];
 
   return (
@@ -61,7 +61,8 @@ export default function Home() {
         <h2 className="text-3xl font-bold mb-20 text-white text-center">
           Encuentra un plan adaptado a tus necesidades
         </h2>
-        <section className="flex flex-wrap gap-20">
+
+        <section className="flex flex-wrap justify-center items-start gap-20">
           {pricingData.map((data) => (
             <PricingCard {...data} />
           ))}
