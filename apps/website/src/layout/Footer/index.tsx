1ea1047258673f3ca302ca545a57d539fc2836fa
kevelscy <kevelscy@gmail.com>
2023-05-11 16:42:58 -0400

    wrap logo devels with link

--- apps/website/src/layout/Footer/index.tsx
+++ apps/website/src/layout/Footer/index.tsx
@@ -4,12 +4,14 @@ import { IoLogoInstagram, IoLogoLinkedin, IoLogoWhatsapp } from "react-icons/io5
 export const Footer = () => {
   return (
     <footer className="flex flex-col items-center w-full max-w-3xl mx-auto py-10 text-white text-center px-8 lg:px-0">
-      <Image 
-        src='/images/logo-devels.png'
-        alt='Devels Technology'
-        width={70}
-        height={70}
-      />
+      <a href="http://devels.tech" target="_blank" rel="noopener noreferrer">
+        <Image 
+          src='/images/logo-devels.png'
+          alt='Devels Technology'
+          width={70}
+          height={70}
+        />
+      </a>
 
       <section className="flex flex-col items-center lg:flex-row lg:justify-between mt-10 w-full">
         <p>© 2023 Devels. Todos los derechos reservados.</p>
